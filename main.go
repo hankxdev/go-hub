837cf5f56cb72fa437d54cb8eefcd521d2061035
Hank X <yrstyh@gmail.com>
2020-07-22 14:29:54 +0800

    add 2 new commands

--- main.go
+++ main.go
@@ -5,18 +5,22 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
+	"strings"
 
 	flag "github.com/ogier/pflag"
 )
 
 var (
-	msg string
+	msg     string
+	branch  string
+	cBranch string
 )
+
+var blankRegex = regexp.MustCompile(`\s+`)
 
 func main() {
 	flag.Parse()
-
-	// args := flag.Args()
 
 	if len(flag.Args()) < 1 && flag.NFlag() == 0 {
 		fmt.Printf("Usage:  %s [options]\n", os.Args[0])
@@ -24,7 +28,52 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+	flagActions(os.Args[1])
+}
 
+func flagActions(arg string) {
+	switch arg {
+	case "-m", "msg":
+		quickPush()
+	case "-b", "branch":
+		quickBranch()
+	case "-c", "checkout":
+		quickCheckout()
+	}
+
+}
+
+func quickBranch() {
+	branch = strings.TrimSpace(branch)
+	if branch == "" {
+		echo("Empty branch name is not allowed")
+		os.Exit(1)
+	}
+	branch = blankRegex.ReplaceAllString(branch, "-")
+	gitBranch := exec.Command("git", "branch", branch)
+	gitCheckoutBranch := exec.Command("git", "checkout", branch)
+	err := gitBranch.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = gitCheckoutBranch.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	echo(fmt.Sprintf("create and switched to branch %s", branch))
+}
+
+func quickCheckout() {
+	gitCheckout := exec.Command("git", "checkout", cBranch)
+
+	err := gitCheckout.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	echo(fmt.Sprintf("Switched to branch %s", cBranch))
+}
+
+func quickPush() {
 	gitAdd := exec.Command("git", "add", ".")
 	gitCommit := exec.Command("git", "commit", fmt.Sprintf("-m %s", msg))
 	gitPush := exec.Command("git", "push")
@@ -49,7 +98,10 @@ func main() {
 }
 
 func init() {
-	flag.StringVarP(&msg, "msg", "m", "new code", "Commit Message")
+	flag.StringVarP(&msg, "msg", "m", "new code", "add all your changed files, commit with the message, then push")
+	flag.StringVarP(&branch, "branch", "b", "new-branch", "create a new branch and switch to it")
+	flag.StringVarP(&cBranch, "checkout", "c", "master", "checkout a branch")
+
 }
 
 func echo(msg string) {
